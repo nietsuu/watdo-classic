c3735fbc4e701a492e4161c507a2342861fdb66c
Nie Tsuu <nietsuu@gmail.com>
2024-01-05 16:31:59 +0800

    Implement ProfileEmbed

--- watdo/discord/cogs/profile.py
+++ watdo/discord/cogs/profile.py
@@ -1,6 +1,7 @@
 from discord.ext import commands as dc
 from watdo.discord import DiscordBot
 from watdo.discord.cogs import BaseCog
+from watdo.discord.embeds import ProfileEmbed
 from watdo.models.profile import Profile as ProfileModel
 
 
@@ -9,7 +10,7 @@ class Profile(BaseCog):
     async def profile(self, ctx: dc.Context) -> None:
         """Show your profile info."""
         profile = await ProfileModel.from_ctx(ctx)
-        await self.send(ctx.channel, profile)
+        await self.send(ctx.channel, embed=ProfileEmbed(self.bot, profile))
 
 
 async def setup(bot: DiscordBot) -> None:
--- watdo/discord/embeds.py
+++ watdo/discord/embeds.py
@@ -1,6 +1,7 @@
 from typing import Any
 import discord
 from watdo.discord import DiscordBot
+from watdo.models.profile import Profile
 
 
 class Embed(discord.Embed):
@@ -9,3 +10,12 @@ class Embed(discord.Embed):
             kwargs["color"] = bot.color
 
         super().__init__(title=title, **kwargs)
+
+
+class ProfileEmbed(Embed):
+    def __init__(self, bot: DiscordBot, profile: Profile) -> None:
+        user = bot.get_user(int(profile.uuid))
+        super().__init__(bot, user.display_name if user else profile.uuid)
+
+        if user is not None:
+            self.set_thumbnail(url=user.display_avatar.url)
